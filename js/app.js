88fee42006e6aaf24e4469bf4f509578615dcf78
Arrick <Grapes01@gmail.com>
2019-03-21 22:44:19 -0700

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//1. Verbal questions
+
+// What is the difference between a parameter and an argument?
+// A. A parameter is a variable withing a method definition
+//an arguement is the actual value that is passed when calling the function.
+
+//Within a function, what is the difference between return and console.log?
+// While console.log will not affect a function, return is a 
+//call to pass a value back based on the function. 
+
+//What are the implications of the ability of a function to return a value?
+// For a function to retun a vale, a specific parameter(s) are called upon 
+// by the statements made within the function. 
